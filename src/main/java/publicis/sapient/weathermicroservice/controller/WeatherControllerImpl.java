ebb2988f6d9a8a763a2df419eb90abe7621419b4
PrashantThakurNitP <prashant09.thakur@gmail.com>
2024-01-18 10:14:28 +0530

    add cors

--- src/main/java/publicis/sapient/weathermicroservice/controller/WeatherControllerImpl.java
+++ src/main/java/publicis/sapient/weathermicroservice/controller/WeatherControllerImpl.java
@@ -15,7 +15,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/v1")
 @Slf4j
-@CrossOrigin(origins = "http://localhost:3000")
+@CrossOrigin(origins = "*")
 public class WeatherControllerImpl implements WeatherController {
 
     @Autowired
