323711974b30a5076ddf42e98a836135d0b6f022
PrashantThakurNitP <prashant09.thakur@gmail.com>
2024-01-20 21:52:20 +0530

    Fix for Bean not creating

--- src/main/java/publicis/sapient/weathermicroservice/utils/WeatherApiCall.java
+++ src/main/java/publicis/sapient/weathermicroservice/utils/WeatherApiCall.java
@@ -2,6 +2,7 @@ package publicis.sapient.weathermicroservice.utils;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import publicis.sapient.weathermicroservice.domain.WeatherApiResponse;
@@ -13,8 +14,12 @@ public class WeatherApiCall {
 
     @Value("${openweathermap.api.url}")
     private String apiUrl;
-    @Autowired
+
     RestTemplate restTemplate;
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public WeatherApiResponse getWeatherInfo(String cityName,int cnt) {
         String url = String.format("%s/?q=%s&appid=%s&cnt=%d", apiUrl, cityName, apiKey,cnt);
