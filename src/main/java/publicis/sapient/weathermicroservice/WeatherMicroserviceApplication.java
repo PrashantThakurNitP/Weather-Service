6ec9c0c00e031b8c13f853d9cf70abd52c65c3d4
PrashantThakurNitP <prashant09.thakur@gmail.com>
2024-02-03 15:32:28 +0530

    add eureka discovery client

--- src/main/java/publicis/sapient/weathermicroservice/WeatherMicroserviceApplication.java
+++ src/main/java/publicis/sapient/weathermicroservice/WeatherMicroserviceApplication.java
@@ -2,10 +2,10 @@ package publicis.sapient.weathermicroservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import publicis.sapient.weathermicroservice.config.SpringFoxConfig;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WeatherMicroserviceApplication {
 
 	public static void main(String[] args) {
